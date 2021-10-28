42e3dd909629f065cc26ee55447ca616c53227fc
Nenashev Daniil <nenashev.daniil.02@gmail.com>
2021-10-28 18:18:46 +0300

    -Initial Commit

---
+++ train/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace train
+{
+    class Program
+        
+
+    {
+        static void Main()
+        {
+            Train c = new Train(5);
+            c.FillIn("mass", 0);
+            c.PutOut(mass,5 , 0);
+
+
+
+
+        }
+    }
+}
---
+++ train/Train.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace train
+{
+    class Train
+    {
+        Vagon[] sostav;
+        public int i;
+       public int a;
+       
+        
+
+
+        public  Train( int a)
+        {
+            sostav= new Vagon[a];
+            
+
+        }
+        public void FillIn(string b, int i)
+        {
+            for (i = 0; i < a; i++)
+            {
+                if (b == "да")
+                    sostav[i].mass = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void PutOut( int mass, int a, int i)
+
+        {
+           
+            for (i=0; i<a ;i++)
+            {
+                Console.WriteLine($"Вагон № {i} отправляется с грузом массой =" , mass );
+            }
+        }
+           
+
+            
+
+
+
+
+    }
+
+   
+
+} 
+
